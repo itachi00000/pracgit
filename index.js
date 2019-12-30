fccb306da4f2e4f1260eb5844120db0a16cc7b51
itachi0 <58568276+itachi00000@users.noreply.github.com>
2019-12-30 16:14:47 +0800

    async await

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-const users = [
-  { id: 1, name: 'Joe' },
-  { id: 2, name: 'Jane' },
-  { id: 3, name: 'John' }
-];
-// const newUser = { id: 10 };
+const promisify = (item, delay) =>
+  new Promise(resolve => setTimeout(() => resolve(item), delay));
 
-const getNames = users.map(({ name }) => {
-  return name;
-});
+const a = () => promisify('a', 3999);
+const b = () => promisify('b', 2000);
+const c = () => promisify('c', 3001);
 
-console.log(getNames);
+async function parallel() {
+  const promises = [a(), b(), c()];
+  const [out1, out2, out3] = await Promise.all(promises);
+  return `parallel is done: ${out1} ${out2} ${out3}`;
+}
+
+async function race() {
+  const promises = [a(), b(), c()];
+  const out1 = await Promise.race(promises);
+  return `race is done ${out1};`;
+}
+
+async function seq() {
+  const out1 = await a();
+  const out2 = await b();
+  const out3 = await c();
+  return `sequence is done: ${out1} ${out2} ${out3}`;
+}
+
+seq().then(console.log);
+parallel().then(console.log);
+race().then(console.log);
