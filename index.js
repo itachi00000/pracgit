c5c3510c1723afb34838bb7abeb8fd31c3566361
itachi0 <58568276+itachi00000@users.noreply.github.com>
2019-12-30 10:10:46 +0800

    Prac ARMD

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const h1 = 'hi'
-const h1= 'hi'
-const h2 = 'hi'
+const users = [
+  { id: 1, name: 'Joe' },
+  { id: 2, name: 'Jane' },
+  { id: 3, name: 'John' }
+];
+// const newUser = { id: 10 };
 
-const h2 =  'h2'
+const getNames = users.map(({ name }) => {
+  return name;
+});
+
+console.log(getNames);
